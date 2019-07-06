ac990310fc707ce3165d08d842513b137ab84ead
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-07-05 22:34:18 -0700

    Can create product

--- accounts/views.py
+++ accounts/views.py
@@ -23,8 +23,25 @@ def signup(request):
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Username or Password is incorrect.'})
+
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+
+        return redirect('home')
+
+
     return render(request, 'accounts/signup.html')
